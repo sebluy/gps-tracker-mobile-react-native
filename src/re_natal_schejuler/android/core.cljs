6be5feabfe32059ddf01295f6f7f83e16e8b7b2b
Sebastian Luy <sebastian.a.luy@gmail.com>
2016-01-19 14:01:43 -0500

    Start expirementing with network

--- src/re_natal_schejuler/android/core.cljs
+++ src/re_natal_schejuler/android/core.cljs
@@ -1,32 +1,45 @@
 (ns ^:figwheel-load re-natal-schejuler.android.core)
 
-(set! js/React (js/require "react-native/Libraries/react-native/react-native.js"))
+(set! js.React (js.require "react-native/Libraries/react-native/react-native.js"))
 
-(def app-registry js/React.AppRegistry)
+(def app-registry js.React.AppRegistry)
 
 (defn alert [title]
-  (.alert (.-Alert js/React) title))
+  (.alert (.-Alert js.React) title))
 
-(def Text (.-Text js/React))
-(def View (.-View js/React))
+(defn component-constructor [type]
+  (fn [props & children]
+    (apply js.React.createElement type (clj->js props) children)))
+
+(def Text (component-constructor (.-Text js.React)))
+(def TouchableHighlight (component-constructor (.-TouchableHighlight js.React)))
+(def View (component-constructor (.-View js.React)))
 
 (defn better-view []
-  [View {:style {:margin 40}}
-   [Text {} "Hello"]
-   [Text {} "Man"]])
-
-(defn tree->components [tree]
-  (if (vector? tree)
-    (let [[type props & children] tree]
-      (apply
-       js/React.createElement
-       type
-       (clj->js props)
-       (map tree->components children)))
-    (clj->js tree)))
+  (View {:style {:margin 40}}
+        (TouchableHighlight {:onPress #(alert "Hi")
+                             :style {:padding 10
+                                     :backgroundColor "#C0C0C0"
+                                     :borderRadius 5}}
+                            (Text {:style {:textAlign "center"}} "Refresh"))
+        (Text {:style {:marginTop 40}} "No Waypoints")))
 
 (defn render []
-  (js/React.render (tree->components (better-view)) 1))
+  (js.React.render (better-view) 1))
+
+(def response (atom nil))
+
+(-> @response)
+
+(-> (js.fetch "https://fierce-dawn-3931.herokuapp.com/api"
+              (clj->js {:method "POST"
+                        :headers {:content-type "application/edn"}
+                        :body (str [{:action :get-paths
+                                     :path-type :waypoint}])}))
+    (.then #(.body %))
+    (.then #(reset! response (cljs.reader/read-string %))))
+
+(cljs.reader/read-string @response)
 
 (render)
 
