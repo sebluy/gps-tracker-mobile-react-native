5b00e50e2a3b5b893f4e6d442e29ac57923bd9d3
Sebastian Luy <sebastian.a.luy@gmail.com>
2016-03-18 21:03:10 -0400

    Add timer

--- src/gps_tracker/core.cljs
+++ src/gps_tracker/core.cljs
@@ -1,8 +1,11 @@
 (ns gps-tracker.core
-  (:require [gps-tracker.react :as r]))
+  (:require [gps-tracker.react :as r]
+            [clojure.string :as s]
+            [gps-tracker.view :as v]))
 
 (declare handle)
 (declare render)
+(declare address)
 
 (defonce state (atom nil))
 (defonce debug (atom {:state '() :actions '()}))
@@ -23,26 +26,100 @@
 ;(js/Object.keys js.React.ToastAndroid)
 
 (defn toast [msg]
-  (js.React.ToastAndroid.show msg js.React.ToastAndroid.LONG))
+  (js.React.ToastAndroid.show msg js.React.ToastAndroid.SHORT))
 
-;(toast "hi")
+(defn watch-position []
+  (let [on-success (fn [position]
+                     (address `(:new-position ~(js->clj position))))
+        on-error toast
+        options #js {:enableHighAccuracy true}]
+    (js.React.Geolocation.watchPosition on-success on-error options)))
 
-;(str (js->clj #js {:hi "you"}))
+(defn clear-watch [id]
+  (js.React.Geolocation.clearWatch id))
 
-;(Object.keys js.React.Geolocation)
-#_(js.React.Geolocation.getCurrentPosition
- (fn [location] (-> location js->clj str toast))
- (fn [error] (toast error))
- #js {:timeout 5000})
+(defn start-tracking [state]
+  (let [interval (js/setInterval #(address `(:tick)) 200)]
+    (assoc state
+           :tracking true
+           :interval interval
+           :now (js/Date.)
+           :started (js/Date.))))
+
+(defn stop-tracking [{:keys [interval] :as state}]
+  (js/clearInterval interval)
+  (-> state
+      (assoc :tracking false)
+      (dissoc :started :now)))
 
 (defn init []
-  {:page {:id :home}})
+  {:tracking false})
 
 (defn handle [action state]
-  state)
+  (case (first action)
+    :start
+    (start-tracking state)
 
-(defn view [address state]
-  (r/text nil "Hello, world!"))
+    :tick
+    (assoc state :now (js/Date.))
+
+    :stop
+    (stop-tracking state)
+
+    state))
+
+(defn button [text on-press]
+  (r/touchable-highlight
+   {:onPress on-press
+    :style [v/styles.button
+            v/styles.goldBorder]}
+   (r/text
+    {:style [v/styles.text
+             v/styles.bigText]}
+    text)))
+
+(defn stop-tracking-button [address]
+  (r/touchable-highlight
+   {:onPress #(address '(:stop))}
+   (r/text nil "Stop Tracking")))
+
+(defn last-position-view [position]
+  (r/text nil (str position)))
+
+(defn pad2-0 [s]
+  (if (= 1 (count s))
+    (str "0" s)
+    s))
+
+(defn duration-str [ms]
+  (let [s (mod (js/Math.floor (/ ms 1000)) 60)
+        m (mod (js/Math.floor (/ ms 60000)) 60)
+        h (mod (js/Math.floor (/ ms (* 60 60 1000))) 24)]
+    (s/join ":" (map (comp pad2-0 str) [h m s]))))
+
+(defn duration [t1 t2]
+  (- (.getTime t2) (.getTime t1)))
+
+
+(defn tracking-view [{:keys [started now]}]
+  (r/view
+   nil
+   (button "Stop Tracking" #(address '(:stop)))
+
+   (r/view
+    {:style [v/styles.timeBox
+             v/styles.goldBorder]}
+    (r/text nil (duration-str (duration started now))))))
+
+(defn view [address {:keys [tracking last-position] :as state}]
+  (r/view
+   {:style [v/styles.purple
+            v/styles.fullPage]}
+   (r/view
+    {:style [v/styles.page]}
+    (if tracking
+      (tracking-view state)
+      (button "Start Tracking" #(address '(:start)))))))
 
 (defn address [action]
   (swap! debug update :actions conj action)
--- src/gps_tracker/view.cljs
+++ src/gps_tracker/view.cljs
@@ -8,6 +8,10 @@
 
 (def GREY "#C0C0C0")
 
+(def PURPLE "#660066")
+
+(def GOLD "#e6bc06")
+
 (def styles
   (js.React.StyleSheet.create
    (clj->js
@@ -15,14 +19,25 @@
               :margin 5
               :borderRadius 5
               :backgroundColor GREY}
+     :goldBorder {:borderWidth 5
+                  :borderColor GOLD}
      :narrowButton {:marginHorizontal 40}
      :text {:textAlign "center"}
-     :bigText {:fontSize 18}
+     :bigText {:fontSize 20}
+     :middle {:top 125}
+     :center {:margin 40}
+     :timeBox {:marginTop 30
+               :marginHorizontal 5
+               :borderRadius 5
+               :backgroundColor GREY
+               :padding 5}
      :toolbar {:height 56
                :backgroundColor GREY}
      :scroll-view {:height 300}
      :marginVertical {:marginVertical 10}
-     :page {:margin 40}})))
+     :purple {:backgroundColor PURPLE}
+     :page {:margin 40}
+     :fullPage {:height 512}})))
 
 (q/defcomponent WaypointPathRow
   [id]
